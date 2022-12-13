ce30a580e33ffdcd6bf845abdffa34fdc143a028
Olivier Roche <olivier@studio-gram.com>
2022-12-13 16:26:55 +0100

    event binding

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -7,16 +7,22 @@ import { Fruits } from 'src/app/fruits';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent {
-  title = 'Mon application';
-  price = 10;
+  title: string = 'Mon application';
+  price: number = 10;
+  total: number = 4;
+  more: boolean = false;
   constructor() {
     new Fruits;
     console.log("test");
+    if(this.total > 3) this.more = true;
   }
   priceTTC(priceExcl: number) {
     return priceExcl * 1.2;
   }
   random() {
     return Math.random();
+  }
+  loadMore() {
+    console.log('afficher plus');
   }
 }
