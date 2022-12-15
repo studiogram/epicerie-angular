81e8183187b11740ac6899aba46fe7e67cb065a8
Olivier Roche <olivier@studio-gram.com>
2022-12-15 13:13:38 +0100

    services produits

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Produit } from 'src/app/interfaces/produit';
+import { ProduitsService } from 'src/app/services/produits.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
-  produits: Produit[] = [
-    {name: 'Cerise', img: '/assets/cerise.jpg', price: 5, stock: 10},
-    {name: 'Citron', img: '/assets/citron.jpg', price: 4, stock: 4},
-    {name: 'Fraise', img: '/assets/fraise.jpg', price: 3, stock: 10},
-    {name: 'Poire', img: '/assets/poire.jpg', price: 2, stock: 4},
-  ]
+export class ProductListComponent implements OnInit{
+  produits: Produit[] = [];
+  constructor(private _produits: ProduitsService) {}
+  ngOnInit(): void {
+    this.produits = this._produits.produits;
+  }
 }
---
+++ src/app/services/produits.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Produit } from '../interfaces/produit';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProduitsService {
+  produits: Produit[] = [
+    {name: 'Cerise', img: '/assets/cerise.jpg', price: 5, stock: 10},
+    {name: 'Citron', img: '/assets/citron.jpg', price: 4, stock: 4},
+    {name: 'Fraise', img: '/assets/fraise.jpg', price: 3, stock: 10},
+    {name: 'Poire', img: '/assets/poire.jpg', price: 2, stock: 4},
+  ];
+  constructor() { }
+  readProduits() {
+    /* ici, on fera une demande à l'API */
+    return this.produits;
+  }
+}
