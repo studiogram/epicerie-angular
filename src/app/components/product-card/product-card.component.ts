eb5e9920f74c004e19afa9b6c4a5fddb3316e9a2
Olivier Roche <olivier@studio-gram.com>
2022-12-16 13:24:16 +0100

    Input()

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Produit } from 'src/app/interfaces/produit';
+import { ProduitsService } from 'src/app/services/produits.service';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
-export class ProductCardComponent {
-  @Input() id?: number;
+export class ProductCardComponent implements OnInit{
+  @Input() produit: Produit;
+  @Input() i: number;
+  constructor(private _produits: ProduitsService) {}
+  ngOnInit(): void {}
+  deleteProduit(i: number) {
+    this._produits.deleteProduit(i);
+  }
 }
